2d161705f8bb6f68a36a1cb03773f5a5ebb86fae
SkylarEade <72280815+SkylarEade@users.noreply.github.com>
2025-11-10 17:43:58 -0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from tensorflow import keras
+import matplotlib as plt
+import numpy as np
+
+class NeuralNetwork:
+    def __init__(self):
+        pass
+
+    def sigmoid():
+        pass
+
+    def forward_propagation():
+        pass
+
+    def calculate_loss():
+        pass
+    
